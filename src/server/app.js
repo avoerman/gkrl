a6ec9262e66f6a817ce2702914f606ebecdd0e58
avoerman <avoerman@allegient.com>
2017-01-29 14:38:20 -0500

    removing unused that was breaking build

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,9 @@
 const express = require('express'),
-  path = require('path'),
-  morgan = require('morgan'),
-  bodyParser = require('body-parser'),
-  races = require('./routes/races'),
-  leaderboard = require('./routes/leaderboard');
+    path = require('path'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    races = require('./routes/races'),
+    leaderboard = require('./routes/leaderboard');
 
 const app = express();
 app.set('port', (process.env.PORT || 3000));
@@ -23,21 +23,21 @@ mongoose.Promise = global.Promise;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 
-  //API
-  app.get('/api/racesummary', races.getRaceSummary);
-  app.get('/api/leaderboardsummary', leaderboard.getLeaderBoardSummary);
+    //API
+    app.get('/api/racesummary', races.getRaceSummary);
+    app.get('/api/leaderboardsummary', leaderboard.getLeaderBoardSummary);
 
 
-  // all other routes are handled by Angular
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../../dist/index.html'));
-  });
+    // all other routes are handled by Angular
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../../dist/index.html'));
+    });
 
-  app.listen(app.get('port'), function () {
-    console.log('Angular 2 Full Stack listening on port ' + app.get('port'));
-  });
+    app.listen(app.get('port'), function () {
+        console.log('Angular 2 Full Stack listening on port ' + app.get('port'));
+    });
 });
 
 module.exports = app;
