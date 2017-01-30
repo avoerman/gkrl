03ea6bc6527fb88786dd19022419d20a0264d5a5
avoerman <avoerman@allegient.com>
2017-01-29 22:22:44 -0500

    race summary syncing to database.

--- src/server/routes/races.js
+++ src/server/routes/races.js
@@ -1,23 +1,64 @@
+const models = require('../models/race.model.js');
+
+const getUniqueRaceIdsFromSplits = (splits) => {
+    return splits.map(item => item.raceNumber._id)
+        .filter((value, index, self) => self.indexOf(value) === index);
+};
+
+const getThisRaceSplitsSorted = (splits, raceId) => {
+    return splits.filter((obj) => {
+        return obj.raceNumber._id === raceId;
+    }).sort((a, b) => {
+        return a.time - b.time
+    });
+};
+
+const getSplitsSansNames = (sortedSplits, names) => {
+    let splitsSans = sortedSplits.filter((o) => {
+        return names.indexOf(o.driverName) === -1;
+    });
+    return {
+        time: splitsSans[0] ? splitsSans[0].time : '',
+        name: splitsSans[0] ? splitsSans[0].driverName : ''
+    };
+};
+
 exports.getRaceSummary = function (req, res) {
-  // replace this with actual db call.
-  res.json([{
-      "raceNumber": 1,
-      "date": "2017-01-26T00:00:00.000Z",
-      "goldName": "Jared Shane",
-      "goldTime": 43.824,
-      "silverName": "Alex Voerman",
-      "silverTime": 43.824,
-      "bronzeName": "Michael Hutcheson",
-      "bronzeTime": 43.863
-    }, {
-      "raceNumber": 2,
-      "date": "2017-01-27T00:00:00.000Z",
-      "goldName": "Alex Voerman",
-      "goldTime": 41.456,
-      "silverName": "Brett Linn",
-      "silverTime": 42.165,
-      "bronzeName": "Michael Hutcheson",
-      "bronzeTime": 43.114
-    }]
-  )
+
+    models.Split
+        .find()
+        .populate('raceNumber')
+        .exec(function (err, splits) {
+            if (err) return console.log(err);
+
+            const uniqueRaces = getUniqueRaceIdsFromSplits(splits);
+
+            let raceSummaryObj = [];
+            for (let raceId of uniqueRaces) {
+
+                //This could probably be done in one function, if I was smarter.
+                const thisRaceSplitsSorted = getThisRaceSplitsSorted(splits, raceId);
+
+                const goldTime = thisRaceSplitsSorted[0].time;
+                const goldName = thisRaceSplitsSorted[0].driverName;
+
+                const silverSplits = getSplitsSansNames(thisRaceSplitsSorted, [goldName]);
+                const bronzeSplits = getSplitsSansNames(thisRaceSplitsSorted, [goldName, silverSplits.name]);
+
+                const summaryObj = {
+                    raceNumber: raceId,
+                    date: thisRaceSplitsSorted[0].raceNumber.date,// this should be safe,
+                    goldTime: goldTime,
+                    goldName: goldName,
+                    silverTime: silverSplits.time,
+                    silverName: silverSplits.name,
+                    bronzeTime: bronzeSplits.time,
+                    bronzeName: bronzeSplits.name
+                };
+
+                raceSummaryObj.push(summaryObj);
+            }
+
+            res.json(raceSummaryObj);
+        });
 };
