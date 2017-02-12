03f2906ba82d2cd1581ecf4a15357f8d69017bd0
avoerman <avoerman@allegient.com>
2017-02-12 15:37:15 -0500

    adding loading masks, fix mobile styling bugs.

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { LeaderboardService } from "../services/leaderboard.service";
+import { TdLoadingService } from '@covalent/core';
+import { setTimeout } from 'timers';
 
 @Component({
     selector: 'app-leaderboard',
     templateUrl: './leaderboard.component.html',
     styleUrls: ['./leaderboard.component.css']
 })
-export class LeaderboardComponent implements OnInit {
+export class LeaderboardComponent implements OnInit, AfterViewInit {
 
     leaderboardSummary: any = [];
 
-    constructor(private leaderboardService: LeaderboardService) {}
+    constructor(private leaderboardService: LeaderboardService,
+                private _loadingService: TdLoadingService) {}
 
     ngOnInit() {
         this.getLeaderboardSummary();
     }
 
+    ngAfterViewInit() {
+        this._loadingService.register('load');
+    }
+
     private getLeaderboardSummary() {
         this.leaderboardService.getLeaderboardSummary().subscribe(leaderboardSummary => {
             this.leaderboardSummary = leaderboardSummary;
+            setTimeout(()=> this._loadingService.resolve('load'), 500);
         });
     }
 }
--- src/app/races/races.component.ts
+++ src/app/races/races.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {RacesService} from "../services/races.service";
-import {AuthService} from '../services/auth-service.service';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { RacesService } from "../services/races.service";
+import { AuthService } from '../services/auth-service.service';
+import { TdLoadingService } from '@covalent/core';
 
 @Component({
     selector: 'app-races',
@@ -8,19 +9,25 @@ import {AuthService} from '../services/auth-service.service';
     styleUrls: ['races.component.scss'],
     providers: [AuthService]
 })
-export class RacesComponent implements OnInit {
+export class RacesComponent implements OnInit, AfterViewInit {
     races: any = [];
 
-    constructor(private racesService: RacesService, private authService: AuthService) {
+    constructor(private racesService: RacesService, private authService: AuthService, private loadingService: TdLoadingService) {
     }
 
     ngOnInit() {
         this.getRaceSummary();
     }
 
+    ngAfterViewInit() {
+        this.loadingService.register('load');
+    }
+
     getRaceSummary() {
         this.racesService.getRaceSummary().subscribe(races => {
             this.races = races;
+            setTimeout(() => this.loadingService.resolve('load'), 500);
         });
     }
+
 }
